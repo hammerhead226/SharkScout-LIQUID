a4cad6bdb528bfa64e48f4b17041924a48da8dfa
Pranav Maringanti <141516835+nootsquared@users.noreply.github.com>
2024-11-18 23:05:55 -0500

    removed default modal

--- expo-router/app/Provider.tsx
+++ expo-router/app/Provider.tsx
@@ -1,7 +1,6 @@
 import { useColorScheme } from 'react-native'
 import { TamaguiProvider, type TamaguiProviderProps } from 'tamagui'
 import { ToastProvider, ToastViewport } from '@tamagui/toast'
-import { CurrentToast } from './CurrentToast'
 import { config } from '../tamagui.config'
 
 export function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'config'>) {
@@ -24,7 +23,6 @@ export function Provider({ children, ...rest }: Omit<TamaguiProviderProps, 'conf
         }
       >
         {children}
-        <CurrentToast />
         <ToastViewport top="$8" left={0} right={0} />
       </ToastProvider>
     </TamaguiProvider>
